4a56dc0afef73a7e70055c180b37efd4cba7d4b7
Sam Goudie <hello@sgoudie.com>
2025-09-04 11:33:04 +0100

    Switched to NEXT_PUBLIC_APP_URL

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,7 +3,7 @@ export type ServerEnv = {
 };
 
 export function getServerEnv(): ServerEnv {
-  const APP_URL = process.env.APP_URL || process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+  const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
   return { APP_URL } as const;
 }
 
